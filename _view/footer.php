d78a97857af830a0c2cc0ed17d9e1da04f00866d
jaflesch <jean.flesch93@gmail.com>
2016-12-03 12:54:35 -0200

    Adicionando rollback nas páginas

--- _view/footer.php
+++ _view/footer.php
@@ -12,6 +12,34 @@
 			</footer>
 		</div>
 		
+		<div class="rollback">
+			<i class="fa fa-arrow-up fa-2x"></i>
+		</div>
+		
 		<!-- JS plugins -->
+		<script type="text/javascript">
+			$(document).ready(function() {
+
+				function checkRb() {
+					var height = $(document).scrollTop();
+					var offset = 300;
+
+					if(height > offset) {
+						$('.rollback').show();
+					}
+					else {
+						$('.rollback').hide();	
+					}
+				}				
+
+				checkRb();
+				$(document).scroll(checkRb);
+
+				$('.rollback').click(function() {
+					var body = $("html, body");
+					body.stop().animate({scrollTop:0}, '250');
+				});
+			});
+		</script>
 	</body>
 </html>
